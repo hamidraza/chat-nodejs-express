ab3f590dfae9c14b903deb3c791826f36508e831
Hamid Raza <contact@hamidraza.com>
2013-09-10 18:10:50 +0530

    save users and messages, UI changes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var mongo = require('mongodb-wrapper');
+var db = mongo.db('localhost', 27017, 'demo_chat');
+
 var express = require("express");
 var app = express();
 var port = 3000;
@@ -6,11 +9,11 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
- 
+
 app.get("/", function(req, res){
     res.render("page");
 });
- 
+
 //app.listen(port);
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
@@ -23,11 +26,22 @@ io.sockets.on('connection', function(socket){
         users[socket.id] = data;
         socket.emit('register', {'success': true, 'id': socket.id});
         socket.emit('message', { message: 'welcome to the chat', username: 'Admin', to: data });
+
+        // save newuser Data - Mongodb
+        var newUser = data;
+        newUser.socket_id = socket.id;
+        db.collection('users').save(newUser);
+
     });
 
     socket.on('send', function (data) {
         data.id = socket.id;
         io.sockets.emit('message', data);
+
+        // Save new Message
+        var newMessage = data;
+        newMessage.socket_id = socket.id;
+        db.collection('messages').save(newMessage);
     });
 
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -5,6 +5,7 @@
         _self.name = false;
         _self.email = false;
         _self.id = false;
+        _self.newMsgCount = 0;
 
         var field = $("#field");
         var sendButton = $("#send");
@@ -26,11 +27,16 @@
 
         _self.start = function(){
             socket.on('message', function (data) {
+                if($('.chat-box').hasClass('hidden-box')){
+                    $('.chat-box-header').text('New Messages ('+(_self.newMsgCount++)+')');
+                    $('.chat-box.hidden-box').addClass('new-message');
+                }
                 if(data.message) {
                     $('<li></li>',{
                         'html': (data.message || 'Empty')+'<small> - '+(data.username || 'guest')+'</small>',
                         'class': (data.id == _self.id? 'you': '')
                     }).appendTo(content);
+                    $('#content').scrollTop($('#content')[0].scrollHeight);
                 } else {
                     console.log("There is a problem:", data);
                 }
@@ -64,10 +70,22 @@
         user.name = $('.username', _this).val();
         user.email = $('.useremail', _this).val();
         _this.remove();
-        $('.chat-box').css('display','block');
+        $('.chat-area').css('display','block');
         user.init();
         return false;
     });
+
+    $('.chat-box-header').on('click', function(){
+        var chatBox = $('.chat-box').removeClass('new-message');
+        $('.chat-box-header').text('Goup chat service');
+        user.newMsgCount = 0;
+        if($('.chat-box-wrapper').is(':visible')){
+            chatBox.addClass('hidden-box');
+        }else{
+            chatBox.removeClass('hidden-box');
+        }
+        $('#content').scrollTop($('#content')[0].scrollHeight);
+    })
 
 
 })(jQuery);
